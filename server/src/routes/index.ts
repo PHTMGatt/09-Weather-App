2f6b3ba1ca52977820da36833d9b80c8e2b2e89b
PHTMGatt <phtmgatt@gmail.com>
2025-02-24 00:38:31 -0500

    push3

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -3,8 +3,8 @@ const router = Router();
 
 import apiRoutes from './api/index.js';
 import htmlRoutes from './htmlRoutes.js';
-
 router.use('/api', apiRoutes);
+
 router.use('/', htmlRoutes);
 
 export default router;
